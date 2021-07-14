bd5be2bf224cb5059ffcfb27e5a4740c0eb02f96
A. Ramzy <ramzy.2871@gmail.com>
2021-07-14 17:56:42 +0200

    Intial commit

---
+++ Football-League/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Football-League
+//
+//  Created by Ramzy on 14/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
